e18ce465ea74cbb8c405b060c3613850089c6baf
Rory Stokes <helix@forscience.com.au>
2013-01-27 15:36:36 +1030

    Fixed arteries directions

--- arteries.js
+++ arteries.js
@@ -52,7 +52,7 @@ cowop.Branch = enchant.Class.create(enchant.Sprite, {
     initialize: function(x, y, width, height, image) {
         enchant.Sprite.call(this, width, height);
         this.image = image;
-        this.frame = 0;
+        this.frame = 1;
         this.x = x;
         this.y = y;
         this.flow  = false;
@@ -64,9 +64,9 @@ cowop.Branch = enchant.Class.create(enchant.Sprite, {
             this.children[i].update();
         }
 	if(this.flow){
-	    this.frame = [1];
-	} else {
 	    this.frame = [0];
+	} else {
+	    this.frame = [1];
 	}
     },
     supply: function(volume) {
@@ -130,30 +130,30 @@ cowop.Arteries = enchant.Class.create(enchant.Sprite, {
         this.aorta.children.push(this.leftBrachial);
         scene.addChild(this.leftBrachial);
         this.leftArm = new cowop.Organ(x+223,y+119,117,33,game.assets["arteries_left_arm.png"]);
-        this.leftBrachial.leftChild = this.leftArm;
+        this.leftBrachial.rightChild = this.leftArm;
         scene.addChild(this.leftArm);
         this.leftBrain = new cowop.Organ(x+371,y+109,20,5,game.assets["arteries_left_brain.png"]);
-        this.leftBrachial.rightChild = this.leftBrain;
+        this.leftBrachial.leftChild = this.leftBrain;
         scene.addChild(this.leftBrain);
         
         this.rightBrachial = new cowop.Junction(x+331,y+51,32,31,game.assets["arteries_right_brachial.png"]);
         this.aorta.children.push(this.rightBrachial);
         scene.addChild(this.rightBrachial);
         this.rightArm = new cowop.Organ(x+214,y+26,126,25,game.assets["arteries_right_arm.png"]);
-        this.rightBrachial.rightChild = this.rightArm;
+        this.rightBrachial.leftChild = this.rightArm;
         scene.addChild(this.rightArm);
         this.rightBrain = new cowop.Organ(x+363,y+64,28,13,game.assets["arteries_right_brain.png"]);
-        this.rightBrachial.leftChild = this.rightBrain;
+        this.rightBrachial.rightChild = this.rightBrain;
         scene.addChild(this.rightBrain);
         
         this.leftIliac = new cowop.Junction(x+206,y+100,30,28,game.assets["arteries_left_iliac.png"]);
         this.aorta.children.push(this.leftIliac);
         scene.addChild(this.leftIliac);
         this.leftLeg = new cowop.Organ(x+22,y+118,184,27,game.assets["arteries_left_leg.png"]);
-        this.leftIliac.leftChild = this.leftLeg;
+        this.leftIliac.rightChild = this.leftLeg;
         scene.addChild(this.leftLeg);
         this.stomach = new cowop.Organ(x+236,y+116,4,4,game.assets["arteries_stomach.png"]);
-        this.leftIliac.rightChild = this.stomach;
+        this.leftIliac.leftChild = this.stomach;
         scene.addChild(this.stomach);
         
         this.rightIliac = new cowop.Junction(x+190,y+60,29,28,game.assets["arteries_right_iliac.png"]);
